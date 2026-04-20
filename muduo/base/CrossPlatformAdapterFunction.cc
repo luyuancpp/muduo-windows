66de677f95f65d5f09076d485d6a3b2b150e779c
luhailoing <lhlcpp6@163.com>
2026-04-20 16:59:28 +0800

    fix: map WSAECONNRESET/WSAECONNABORTED to ECONNRESET in Windows socket wrappers
    winreadsock/winwritesock did not translate Windows socket errors
    WSAECONNRESET (10054) and WSAECONNABORTED (10053) to POSIX ECONNRESET,
    causing TcpConnection::sendInLoop to log noisy ERROR on normal client
    disconnects.

--- muduo/base/CrossPlatformAdapterFunction.cc
+++ muduo/base/CrossPlatformAdapterFunction.cc
@@ -320,6 +320,10 @@ ssize_t winreadsock(int fd, void *buf, size_t count)
         {
             errno = EWOULDBLOCK;
         }
+        else if (err == WSAECONNRESET || err == WSAECONNABORTED)
+        {
+            errno = ECONNRESET;
+        }
         else
         {
             _set_errno(err);
@@ -340,6 +344,10 @@ ssize_t winwritesock(int fd, const void *buf, size_t count)
         if (err == WSAEWOULDBLOCK)
         {
             errno = EWOULDBLOCK;
+        }
+        else if (err == WSAECONNRESET || err == WSAECONNABORTED)
+        {
+            errno = ECONNRESET;
         }
         else
         {
